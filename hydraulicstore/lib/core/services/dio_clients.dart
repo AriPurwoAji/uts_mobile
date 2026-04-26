12cb66d20fe6f79bf8a661def7dcdc46a8efdb12
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-26 16:35:22 +0700

    add dio_clients.dart

---
+++ hydraulicstore/lib/core/services/dio_clients.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import '../constants/api_constants.dart';
+import 'secure_storage.dart';
+
+class DioClient {
+  static Dio? _dio;
+
+  static Dio get instance {
+    _dio ??= _createDio();
+    return _dio!;
+  }
+
+  static Dio _createDio() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(milliseconds: ApiConstants.connectTimeout),
+        receiveTimeout: const Duration(milliseconds: ApiConstants.receiveTimeout),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final token = await SecureStorage.getToken();
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+          return handler.next(options);
+        },
+        onError: (error, handler) {
+          return handler.next(error);
+        },
+      ),
+    );
+
+    return dio;
+  }
+}
