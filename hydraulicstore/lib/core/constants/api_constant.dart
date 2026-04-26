0c098a8d9ba4d2d98da2ed9a8f53dc8380977eb8
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-26 16:32:11 +0700

    add api_constant.dart

---
+++ hydraulicstore/lib/core/constants/api_constant.dart
@@ -0,0 +1,15 @@
+class ApiConstants {
+  // Ganti dengan IP address komputer kamu
+  // Cek IP dengan command: ipconfig
+  static const String baseUrl = 'http://10.0.2.2:8080/v1';
+
+  // Auth endpoints
+  static const String verifyToken = '/auth/verify-token';
+
+  // Product endpoints
+  static const String products = '/products';
+
+  // Timeout
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
+}
