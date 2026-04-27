4f6dcefe0ea16c278c86f33d3eb167f9e8c2f318
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-27 19:41:17 +0700

    menghapus file

--- hydraulicsotre/android/app/src/main/kotlin/com/example/hydraulicsotre/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hydraulicsotre
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- hydraulicstore/lib/core/constants/api_constants.dart
+++ hydraulicstore/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   // Ganti dengan IP address komputer kamu
   // Cek IP dengan command: ipconfig
-  static const String baseUrl = 'http://10.5.48.24:8080/v1';
+  static const String baseUrl = 'http://192.168.0.123:8080/v1';
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
