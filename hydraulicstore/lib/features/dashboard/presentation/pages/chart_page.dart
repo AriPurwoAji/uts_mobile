8948ee0b7671ee4e94759c93f1372f458f4d733e
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-27 21:54:00 +0700

    y

---
+++ hydraulicstore/lib/features/dashboard/presentation/pages/chart_page.dart
@@ -0,0 +1,8 @@
+class ChartPage extends StatelessWidget {
+  const ChartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Chart Page"));
+  }
+}
