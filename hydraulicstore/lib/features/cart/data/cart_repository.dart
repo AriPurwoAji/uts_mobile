77cf6e0273268e8a956c1ac73460232c4e84f9aa
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-29 18:44:55 +0700

    add cart_repository.dart

---
+++ hydraulicstore/lib/features/cart/data/cart_repository.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import '../../../core/constants/api_constants.dart';
+import '../../../core/services/dio_clients.dart';
+import '../domain/cart_model.dart';
+
+class CartRepository {
+  final DioClient _dioClient;
+  CartRepository(this._dioClient);
+
+  Future<CartModel> getCart() async {
+    try {
+      final response = await _dioClient.dio.get(ApiConstants.cart);
+      return CartModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Gagal memuat cart');
+    }
+  }
+
+  Future<CartModel> addItem(int productId, int quantity) async {
+    try {
+      final response = await _dioClient.dio.post(
+        ApiConstants.cartItems,
+        data: {'product_id': productId, 'quantity': quantity},
+      );
+      return CartModel.fromJson(response.data['data']);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Gagal menambah item');
+    }
+  }
+
+  Future<void> removeItem(int itemId) async {
+    try {
+      await _dioClient.dio.delete(ApiConstants.cartItemById(itemId));
+    } on DioException catch (e) {
+      throw Exception(e.response?.data['message'] ?? 'Gagal menghapus item');
+    }
+  }
+}
